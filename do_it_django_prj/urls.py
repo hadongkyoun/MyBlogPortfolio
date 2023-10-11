b380222d0b81edef290ca43d1b630638cdaf3cb9
devKyoun <gkehdrbs36@naver.com>
2023-10-11 17:44:30 +0900

    대문 페이지와 자기소개 페이지 만들기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -12,13 +12,14 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('single_pages/', include('single_pages.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # 방문자가 blog/로 접속할때, blog 앱 폴더의 urls.py를 참고하라
+    # 방문자가 single_pages/로 접속할때, single_pages 앱 폴더의 urls.py를 참고하라
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
 ]
